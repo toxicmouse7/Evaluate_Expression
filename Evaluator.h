c3cdc26410336f95fdedb663556bf726840d4514
toxicmouse7 <utiha@sasuke.ru>
2021-11-24 12:10:03 +0500

    Rewritten

--- Evaluator.h
+++ Evaluator.h
@@ -4,10 +4,15 @@
 
 #ifndef EVALUATE_EXPRESSION_EVALUATOR_H
 #define EVALUATE_EXPRESSION_EVALUATOR_H
+
 #include <string>
+#include <utility>
 #include <vector>
 #include <stack>
 #include <map>
+#include <unordered_map>
+#include <functional>
+#include <cmath>
 
 class Evaluator
 {
@@ -21,39 +26,91 @@ private:
             constant,
             openBracket,
             closeBracket,
-            operation,
-            none
+            operation
         };
 
-        term_t type = none;
+        term_t type;
         std::string name;
+
+        Term(std::string s, term_t t) : name(std::move(s)), type(t)
+        {}
     };
+
     std::vector<Term> terms;
-    std::map<std::string, int> priorities = {
-            {"+", 0},
-            {"-", 0},
-            {"*", 1},
-            {"/", 1},
+    std::unordered_map<std::string, int> priorities = {
+            {"+",  0},
+            {"-",  0},
+            {"*",  1},
+            {"/",  1},
             {"//", 1},
-            {"%", 1},
-            {"&", 1},
-            {"^", 1},
+            {"%",  1},
+            {"&",  1},
+            {"^",  1},
             {"**", 2}
     };
 
-    void to_postfix();
-    void string_to_terms(std::string& expr);
-    void type_terms();
+    std::map<std::string, std::function<double(std::stack<double> &)>> funs
+            {
+                    {"+",           [](std::stack<double> &s)
+                                    { return pop(s) + pop(s); }},
+                    {"-",           [](std::stack<double> &s)
+                                    { return -pop(s) + pop(s); }},
+                    {"sin",         [](std::stack<double> &s)
+                                    { return std::sin(pop(s)); }},
+                    {"/",           [](std::stack<double> &s)
+                                    { return 1.0 / pop(s) * pop(s); }},
+                    {"*",           [](std::stack<double> &s)
+                                    { return pop(s) * pop(s); }},
+                    {"!",           [](std::stack<double> &s)
+                                    { return !(bool) pop(s); }},
+                    {"factorial",   [](std::stack<double> &s)
+                                    {
+                                        auto p = pop(s);
+                                        return std::tgamma((int) p + 1);
+                                    }},
+                    {"^",           [](std::stack<double> &s)
+                                    {
+                                        auto p = pop(s);
+                                        return pow(pop(s), p);
+                                    }},
+                    {"unary_minus", [](std::stack<double> &s)
+                                    { return -pop(s); }}
+            };
 
-    static bool is_number(std::string& s);
-    static bool is_numeric(const Term& t);
-    bool priority(const Term& t1, const Term& t2);
-    static Term evaluate_function(Term& term, std::string& arg);
-    static Term evaluate_operation(Term& term, std::string& arg1, std::string& arg2);
+    std::map<std::string, int> priority{{"+",           10},
+                                        {"-",           10},
+                                        {"*",           20},
+                                        {"/",           20},
+                                        {"^",           30},
+                                        {"unary_minus", 10},
+                                        {"!",           25},
+                                        {"factorial",   30}};
+
+
+    static double pop(std::stack<double> &s)
+    {
+        auto val = s.top();
+        s.pop();
+        return val;
+    };
+
+    std::vector<Evaluator::Term> get_terms_from_string(const std::string &expr);
+
+    std::vector<Evaluator::Term> get_postfix_from_terms();
+
+    bool is_numeric(const Evaluator::Term &t);
+
+    double get_value_from_postfix(std::map<std::string, double> &vars,
+                                  std::map<std::string, std::function<double(
+                                          std::stack<double> &)>> &user_functions);
+
 public:
-    explicit Evaluator(std::string& expr);
-    double evaluate();
-    void show();
+    explicit Evaluator(std::string &expr);
+
+    double evaluate(std::map<std::string, double> &vars,
+                    std::map<std::string, std::function<double(
+                            std::stack<double> &)>> &user_functions);
+
 };
 
 
